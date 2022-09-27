042fbbc1d9dcd0c33987f69b524231f3f51b4527
Bobacar BOUTAYEB <bobacar.boutayeb@le-campus-numerique.fr>
2022-09-27 09:27:50 +0200

    Changing name Computer

--- sshCopyKeyToVM.sh
+++ sshCopyKeyToVM.sh
@@ -1,2 +1,2 @@
-ip=$(sudo virsh net-dhcp-leases default | grep "unassigne" | cut -d/ -f1 | cut -d\  -f16)
+ip=$(sudo virsh net-dhcp-leases default | grep "runner" | cut -d/ -f1 | cut -d\  -f16)
 ssh-copy-id -i $HOME/.ssh/id_ed25519.pub user@$ip
